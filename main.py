f719b6296f4cd6e63d9c9afd98fd1c618ba4d34f
DANIEL POTAPO <danielpotapo@college.harvard.edu>
2026-05-24 19:14:13 -0500

    datasets annotated

--- main.py
+++ main.py
@@ -2,17 +2,26 @@ import json
 from datasets import load_dataset
 
 with open("en_single_choice_test_2K.jsonl", encoding="utf-8") as f:
-    math_dataset = [json.loads(line) for line in f]
+    math_dataset = [json.loads(line) for line in f]         # competition math mcqs
 
-handwriting_dataset = load_dataset("Teklia/IAM-line")
-medical_dataset = load_dataset("openlifescienceai/medmcqa") 
-finance_dataset = load_dataset("luojunyu/FinMME")
+handwriting_dataset = load_dataset("Teklia/IAM-line")       # handwriting identification
+medical_dataset = load_dataset("openlifescienceai/medmcqa") # medical mcqs, based on mcat
+finance_dataset = load_dataset("luojunyu/FinMME")           # financial mcqs
+
+print(math_dataset[0]["problem"])   # "problem", "answer-value", "answer-option-list", "difficulty"
+
+print(medical_dataset["train"][0]["question"])  # "question", "opa", "opb", "opc", "opd", 
+                                                #  "cop" (correct option), "choice-type"
+
+print(finance_dataset["train"][0]["question_text"])  # "image", "question_text", "options", "answer"
+
+print(handwriting_dataset["train"][0]["image"])  # "image", "text"
 
 
-print(math_dataset["train"][0])
-print(handwriting_dataset["train"][0])
-print(medical_dataset["train"][0])
-print(finance_dataset["train"][0])
+
+
+
+
 
 
 
